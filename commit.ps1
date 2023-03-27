4921c825d8c1d9093233c80bee527e98a976c6ca
chetan_desai <chetand@vedlogic.com>
2023-03-27 15:31:11 +0530

    2 new commit

---
+++ commit.ps1
@@ -0,0 +1,14 @@
+# Set the working directory to the location of the repository
+$repoPath = "C:\path\to\repo"
+#cd $repoPath
+
+# Get the list of commits and their timestamps
+$commits = git log --format="%H %ct" --no-merges
+
+# Calculate the lead time to change for each commit
+foreach ($commit in $commits) {
+    $commitHash, $commitTime = $commit.Split(" ")
+    $leadTime = git log --pretty=format:"%ct" -n 2 $commitHash | %{($_ - $commitTime) / 60}
+    Write-Output "Commit $commitHash : Lead time to change = $leadTime minutes"
+}
+
