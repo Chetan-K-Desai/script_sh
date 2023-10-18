50b9ad7e247d619b402fff9beb211efab70b6776
Chetan-K-Desai <117310263+Chetan-K-Desai@users.noreply.github.com>
2023-10-18 14:37:55 +0530

    Update choco.ps1
    add new

--- choco.ps1
+++ choco.ps1
@@ -1,7 +1,7 @@
 # Set directory for installation - Chocolatey does not lock
 # down the directory if not the default
 $InstallDir='C:\ProgramData\chocoportable'
-$env:ChocolateyInstall="$InstallDir"
+#$env:ChocolateyInstall="$InstallDir"
 
 # If your PowerShell Execution policy is restrictive, you may
 # not be able to get around that. Try setting your session to
